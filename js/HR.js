4c8cda444119d975159fbaf1daec1dbe70b5e01d
hana <hsaeed0014@stu.kau.edu.sa>
2022-09-15 08:16:13 +0300

    Create Events Page, Events Details Page and Downloading Template Page

---
+++ js/HR.js
@@ -0,0 +1,23 @@
+let lis = document.querySelectorAll(".hrListTitle");
+let cards = document.querySelectorAll(".hrCard");
+let counter = document.querySelectorAll(".pageNumCounter");
+
+lis.forEach(function(ele){
+    ele.onclick = function(){
+        lis.forEach(function(ele){
+            ele.classList.remove("active");
+        })
+        this.classList.add("active");
+    }
+})
+
+counter.forEach(function(ele){
+    ele.onclick = function(){
+        counter.forEach(function(ele){
+            ele.classList.remove("active");
+        })
+        this.classList.add("active");
+    }
+})
+
+
